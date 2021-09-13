be8d28d044d506de0cd76797384a0f7e302ef9cb
MAXPOL <maxrol32@gmail.com>
2021-09-13 23:31:34 +0300

    Create zabbixInstallCentOS7.sh

---
+++ zabbixInstallCentOS7.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+passwoddb=''
+
+echo "Enter new password database:"
+read $passworddb
+
+yum update -y
+yum install -y epel-release
+yum install -y httpd mariadb-server mariadb wget nano yum-utils
+yum install -y php php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json
+
+
+systemctl enable httpd
+systemctl start httpd
+systemctl enable mariadb
+systemctl start mariadb
+
+mysql_secure_installation <<EOF
+y
+$passworddb
+$passworddb
+y
+n
+y
+y
+EOF
+
+rpm -ivh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm
+
+yum install -y zabbix-server-mysql zabbix-web-mysql zabbix-get
+
+mysql -u root -p$passworddb -e 'create database zabbix;'
+mysql -u root -p$passworddb -e 'create user 'zabbix'@'%' identified BY '$passworddb';'
+mysql -u root -p$passworddb -e 'create user 'grant all privileges on zabbix.* to zabbix@%;'
+mysql -u root -p$passworddb -e 'flush privileges;'
+
+zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u root -p$namdedb zabbix
+
+sed -i 's/# DBPassword=/DBPassword=$passworddb/g' /etc/zabbix/zabbix_server.conf
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+sed -i 's/# php_value date.timezone Europe\/Riga/php_value date.timezone Europe\/Moscow/g' /etc/httpd/conf.d/zabbix.conf
+
+systemctl enable zabbix-server
+reboot
