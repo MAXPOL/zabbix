289a639db16bcb2a308d99e384dd0b4eb02cc4e4
MAXPOL <maxrol32@gmail.com>
2021-09-23 22:41:10 +0300

    Create zabbixAgentInstallCentOS7.sh

---
+++ zabbixAgentInstallCentOS7.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+serveraddress=''
+
+echo "Enter ip address you zabbix server:"
+read serveraddress
+
+yum update -y
+
+rpm -ivh https://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm
+
+yum install -y zabbix-agent
+
+zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -u zabbix -p$passworddb zabbix
+
+sed -i 's/Server=127.0.0.1/Server='$serveraddress'/g' /etc/zabbix/zabbix_agent.conf
+sed -i 's/# ListenPort=10050/ListenPort=10050/g' /etc/zabbix/zabbix_agent.conf
+sed -i 's/ServerActive=127.0.0.1/ServerActive='$serveraddress'/g' /etc/zabbix/zabbix_agent.conf
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+
+systemctl enable zabbix-agent
+
+firewall-cmd --permanent --zone=public --add-port=10050/tcp
+
+echo "Done! After reload enter zabbix agent run"
+
+sleep 10
+
+reboot
